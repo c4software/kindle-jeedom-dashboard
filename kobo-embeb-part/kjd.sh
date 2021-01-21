b3e0e835b8439eb24fde8d01734308baf86db3be
Brosseau Valentin <c4software@gmail.com>
2021-01-21 21:28:19 +0100

    Update kjd.sh

--- kobo-embeb-part/kjd.sh
+++ kobo-embeb-part/kjd.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 killall -TERM nickel hindenburg sickel fickel
 
-while true  
-do  
-  fbink -g file=/mnt/onboard/kindle2.png
-  sleep 1800  
+# Change CPU Scalling
+echo powersave | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+while true
+do
+  wget -O /tmp/display.png http://192.168.1.32/kindle/
+  fbink -g file=/tmp/display.png
+  sleep 600
+  # sleep 1800
 done
